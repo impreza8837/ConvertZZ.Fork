431d954fb0b9a675468c785f710bd24c7aba9812
TaMAki <lancerevo.gtsport@gmail.com>
2022-02-08 02:22:14 +0800

    遷移 Code 內容

---
+++ ConvertZZ/Views/AboutView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ConvertZZ.Views {
+    /// <summary>
+    /// AboutView.xaml 的互動邏輯
+    /// </summary>
+    public partial class AboutView : Window {
+        public AboutView() {
+            InitializeComponent();
+            Version.Content = "v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void Label_MouseUp(object sender, MouseButtonEventArgs e) {
+            Process.Start("https://github.com/flier268/ConvertZZ");
+        }
+    }
+}
